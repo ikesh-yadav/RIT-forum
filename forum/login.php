3bcac6c57a432792dcf3159cd460c1b974f1b000
21-aman <56262431+21-aman@users.noreply.github.com>
2019-11-14 20:52:04 +0530

    qw

--- forum/login.php
+++ forum/login.php
@@ -21,7 +21,7 @@
         $password = "";
         $server="localhost";
         $db_name="forum";
-        /*try to connect to MySQL database */
+        /*trying to connect to MySQL database */
         $link = mysqli_connect($server, $username, $password, $db_name);
         if (mysqli_connect_errno()) {
             printf("Connect failed: %s\n", mysqli_connect_error());
