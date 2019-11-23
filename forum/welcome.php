a225e6b7d5a77ce0f801f2e564eae64240d22a76
ikesh-yadav <ikeshyadav1@gmail.com>
2019-11-23 12:25:17 +0530

    day 6

--- forum/welcome.php
+++ forum/welcome.php
@@ -9,12 +9,22 @@
   <link rel="stylesheet" type="text/css" href="common.css">
 </head>
 <body>
-<?php
-  echo '<div id="header" style ="height:5%;width:100%;background-color: rgb(0, 0, 51);font-size:0px;">';
-  echo '<div id="title" style="width:100%;text-align:center;color:white"><b>Forum</b></div>';
-  echo '<div id="navigation" style="width:100%;"><span style="font-size:15px;float:right;">WELCOME, '.$_SESSION['username'].'</span></div>';
-  echo '</div>';
 
+<div class="flex-container">
+
+  <div id="header">
+    <h1>Forum</h1>
+    <div id="navigation">
+      <div id="createThreadButton"><a href="createThread.php">Create Thread</a></div>
+      <div id="navigationButton"><?php echo $_SESSION['username']?>
+        <ul>
+          <li><a href="viewUser.php">Acccount Information</a></li>
+          <li><a href="logout">Logout</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>
+  <?php
   //values required to setup connect
   if(!isset($_SESSION['logged_in'])){
     header("Location: login.php");
@@ -28,16 +38,19 @@
       echo '<table id="categories">';
       echo '<tr><td>categories</td></tr>';
       if ($result && $result->num_rows> 0) { 
-      //output data of each row
+        //output data of each row
         while($row = $result->fetch_assoc()) {
-          echo "<td><form id=category".$row['id']." method='get' action='viewCategory.php'><input type='hidden' name='categoryId' value='".$row['id']."'></form><a href='#' onclick='document.forms[\"category".$row['id']."\"].submit();'>".$row['name']."</a></td></tr>";
+          echo "<tr><td>
+            <form id=category".$row['id']." method='get' action='viewCategory.php'>
+              <input type='hidden' name='categoryId' value='".$row['id']."'>
+            </form>
+            <a href='#' onclick='document.forms[\"category".$row['id']."\"].submit();'>".$row['name']."</a>
+            </td></tr>";
         }
         mysqli_free_result($result);
       }else echo "<tr><td>No categories</td></tr>";
       echo "</table>";
-
-    echo "<form action='logout.php' method='POST'><input type='submit' class='submitbtn' value='Log Out'/></form>";
-    
+    /*closing the connection to the mysql server created in the 'mysql.php' file*/
     mysqli_close($link);
   }
 ?>
